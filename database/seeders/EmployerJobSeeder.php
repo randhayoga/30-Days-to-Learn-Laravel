f3d9e408ea135f20a55b0408a9acc2405df98b55
Randha Yoga <92174528+randhayoga@users.noreply.github.com>
2025-12-24 11:19:01 +0700

    day 15: db seeders

--- database/seeders/EmployerJobSeeder.php
+++ database/seeders/EmployerJobSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Employer;
 use App\Models\Job;
+use App\Models\Tag;
 
 class EmployerJobSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class EmployerJobSeeder extends Seeder
     public function run(): void
     {
         $employers = Employer::factory()->count(5)->create();
-        Job::factory()->count(50)->recycle($employers)->create();
+        $jobs = Job::factory()->count(50)->recycle($employers)->create();
+        $tags = Tag::factory()->count(4)->create();
+
+        foreach ($jobs as $job) {
+            $randomTags = $tags->random(2)->pluck('id');
+            $job->tags()->attach($randomTags[0]);
+            $job->tags()->attach($randomTags[1]);
+        }
     }
 }
